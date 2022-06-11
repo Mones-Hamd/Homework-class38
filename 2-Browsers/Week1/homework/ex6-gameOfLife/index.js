cec0c3392328f385d8556d3819167e8d53f37add
Mones Hamd <mones.m.azzam91@gmail.com>
2022-06-11 08:28:43 +0200

    solve prep exercise

--- 2-Browsers/Week1/homework/ex6-gameOfLife/index.js
+++ 2-Browsers/Week1/homework/ex6-gameOfLife/index.js
@@ -13,10 +13,12 @@ const NUM_ROWS = 40;
 // life or death
 function createCell(x, y) {
   const alive = Math.random() > 0.5;
+  const lifeTime = alive ? 1 : 0;
   return {
     x,
     y,
     alive,
+    lifeTime,
   };
 }
 
@@ -56,7 +58,12 @@ function createGame(context, numRows, numColumns) {
 
     if (cell.alive) {
       // Draw living cell inside background
-      context.fillStyle = `rgb(24, 215, 236)`;
+
+      let opacity = cell.lifeTime / 4;
+      if (opacity > 1) {
+        opacity = 1;
+      }
+      context.fillStyle = `rgba(24, 215, 236,${opacity})`;
       context.fillRect(
         cell.x * CELL_SIZE + 1,
         cell.y * CELL_SIZE + 1,
@@ -102,12 +109,15 @@ function createGame(context, numRows, numColumns) {
       if (numAlive === 2) {
         // Living cell remains living, dead cell remains dead
         cell.nextAlive = cell.alive;
+        cell.lifeTime++;
       } else if (numAlive === 3) {
         // Dead cell becomes living, living cell remains living
         cell.nextAlive = true;
+        cell.lifeTime = 1;
       } else {
         // Living cell dies, dead cell remains dead
         cell.nextAlive = false;
+        cell.lifeTime = 0;
       }
     });
 
